b46cac8d146da53e41bc7f29f92ec3be3d45447e
Mahesh <MaheshSankaran@users.noreply.github.com>
2024-02-09 11:57:13 +0530

    Update msg_st2.py

--- actions/msg_st2.py
+++ actions/msg_st2.py
@@ -1,6 +1,15 @@
 import sys
 from st2common.runners.base_action import Action
-class MyEchoAction(Action):
-    def run(self, message):
-        print(message)
 
+class MyEchoAction(Action):
+    def run(self, message, subject):
+        print("Subject:", subject)
+        print("Message:", message)
+
+        # Check if subject contains "Zabbix agent"
+        if "Zabbix agent" in subject:
+            self.handle_zabbix_agent_subject(subject)
+
+    def handle_zabbix_agent_subject(self, subject):
+        # Implement logic for handling subject containing "Zabbix agent"
+        print("Handling Issues for 'Zabbix agent':", subject)
